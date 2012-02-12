1fca7d985032f5e19cc3ec5a20f42656bf9e779b
Manuel Huber <Manuel.h87@gmail.com>
2012-02-12 09:21:50 +0100

    blkasf

--- src/filetype.py
+++ src/filetype.py
@@ -24,17 +24,38 @@ class InvalidPathException(FileTypeException):
 
 def ExtGuesser(QObject):
     
+    _TAVI = "AVI"
+    _MMP4 = "video/mp4"
+    _MFLV = "video/x-flv"
+    _TFLV = "Macromedia Flash Video"
+    
+    
     def __init__(self, path):
         QObject.__init__(self)
         self._path = path
         self._proc = QProcess()
         self._proc.finished.connect(self._finished)
     
-    def _getExtension(self, fout):
-        pass
+    def _getExtension(self, text, mime):
+        if text.find(self._TAVI) >= 0:
+            return "avi"
+        elif mime.find(self._MMP4) >= 0:
+            return "mp4"
+        elif ((mime.find(self._MFLV) >= 0) and 
+              (text.find(self._TFLV) >= 0)):
+            return "flv"
+        else:
+            return "unk"
     
     def __call__(self):
         self._proc.start("file", ["-b", path])
-        self._proc.waitForFinished()
-        data = self._proc.readAllStandardOutput()
-        
+        ret0 = self._proc.waitForFinished()
+        textual = self._proc.readAllStandardOutput()
+        self._proc.start("file", ["-bi", path])
+        ret1 = self._proc.waitForFinished()
+        mime = self._proc.readAllStandardOutput()
+        if ret0 and ret1:
+            return self._getExtension(textual, mime)
+        else:
+            # TODO: raise an Exception
+            pass
